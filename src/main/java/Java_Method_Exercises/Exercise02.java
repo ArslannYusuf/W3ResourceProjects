f040c35fe7eb84cf8c4b3b2d54e49bdff066f35c
Yusuf Arslan <yeed060118@gmail.com>
2023-10-01 17:49:56 +0300

    Java Method Exercise

---
+++ src/main/java/Java_Method_Exercises/Exercise02.java
@@ -0,0 +1,39 @@
+package Java_Method_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+
+
+    /*
+    Write a Java method to compute the average of three numbers.
+    Test Data:
+    Input the first number: 25
+    Input the second number: 45
+    Input the third number: 65
+    Expected Output:
+
+    The average value is 45.0
+     */
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number");
+        double firstNumber = scan.nextDouble();
+        System.out.println("Enter first number");
+        double secondNumber = scan.nextDouble();
+        System.out.println("Enter first number");
+        double thirdNumber = scan.nextDouble();
+
+        System.out.println("The average value is " + averageOfThreeNumbers(firstNumber, secondNumber, thirdNumber));
+
+    }
+
+    public static double averageOfThreeNumbers(double firstNumber, double secondNumber, double thirdNumber) {
+
+        double average = (firstNumber + secondNumber + thirdNumber) / 3;
+        return average;
+    }
+
+
+}
